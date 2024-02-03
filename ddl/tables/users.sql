888c44add23527fb3ba26a72308553081a89bb5d
Yevlovy <122384762+Yevlovy@users.noreply.github.com>
2024-02-03 23:21:48 +0200

    MM-2 MM-56 MM-57 Design and implement User model (#3)
    * MM-55 add test db
    
    * added ddls for users and users_quality tables
    
    * MM-56 MM-57 add User models and SQLite configuration
    
    ---------
    
    Co-authored-by: Bohdan Solianyk <bohdan_solianyk@epam.com>

---
+++ ddl/tables/users.sql
@@ -0,0 +1,37 @@
+-- users table keeps initial data
+
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    phone INTEGER,
+    country TEXT,
+    --is_premium INTEGER
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Unique compound index
+CREATE UNIQUE INDEX idx_user_id_username_unique ON users(user_id, username);
+
+-- users_quality / users_characteristics
+CREATE TABLE users_quality (
+    user_id INTEGER PRIMARY KEY,
+    current_weight REAL,
+    height REAL,
+    waist_measurement REAL,
+    chest_measurement REAL,
+    hip measurement REAL,
+    body_type TEXT,
+--    desired_weight REAL,
+--    number_of_people INTEGER,
+    measurement_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Index
+CREATE INDEX idx_user_id ON users(user_id);
+
+
